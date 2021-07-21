0b2445b6122c7f4d8f893ed674a16855302ddaf2
aliev.faramoz <faramoz.aliev@devexpress.com>
2021-07-21 15:49:10 +0300

    Update examples

--- CS/AutofacDI/MainWindow.xaml.cs
+++ CS/AutofacDI/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using DevExpress.Mvvm.DataAnnotations;
 using DevExpress.Mvvm.POCO;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Windows;
 
 namespace AutofacDI {
@@ -10,29 +9,46 @@ namespace AutofacDI {
         public MainWindow() => InitializeComponent();
     }
 
-    [POCOViewModel]
-    public class MainViewModel {
-        public virtual IDetailViewModel DetailViewModel { get; set; }
-        public MainViewModel(IDetailViewModel detailViewModel) => DetailViewModel = detailViewModel;
+    public interface ILogger {
+        ObservableCollection<string> Logs { get; set; }
+        string GetSignature();
+    }
+    public class Logger : ILogger {
+        public ObservableCollection<string> Logs { get; set; } = new ObservableCollection<string>();
+        public string GetSignature() => "From Logger class";
     }
 
-    public interface IDetailViewModel {
-        public ObservableCollection<string> Items { get; set; }
-    }
     [POCOViewModel]
-    public class DetailViewModel : IDetailViewModel {
-        public virtual ObservableCollection<string> Items { get; set; }
-        public DetailViewModel() => Items = new(Enumerable.Range(0, 10).Select(i => "String " + i));
+    public class MainViewModel {
+        ILogger logger;
+        public ObservableCollection<string> Logs => logger.Logs;
+        public MainViewModel(ILogger logger) => this.logger = logger;
+    }
+
+    [POCOViewModel]
+    public class LoggerViewModel {
+        ILogger logger;
+        public virtual string Text { get; set; }
+        public LoggerViewModel(ILogger logger) => this.logger = logger;
+        public void PushText() {
+            AddStringToLog(Text);
+            Text = string.Empty;
+        }
+        public bool CanPushText() => !string.IsNullOrEmpty(Text);
+        public void PushSignature() => AddStringToLog(logger.GetSignature());
+        void AddStringToLog(string str) => logger.Logs.Add(str);
     }
 
     public class ServiceLocator {
         readonly IContainer container;
-        public MainViewModel ViewModel => (MainViewModel)container.Resolve(ViewModelSource.GetPOCOType(typeof(MainViewModel)));
+        public MainViewModel MainViewModel => (MainViewModel)container.Resolve(ViewModelSource.GetPOCOType(typeof(MainViewModel)));
+        public LoggerViewModel LoggerViewModel => (LoggerViewModel)container.Resolve(ViewModelSource.GetPOCOType(typeof(LoggerViewModel)));
 
         public ServiceLocator() => container = BuildUpContainer();
         IContainer BuildUpContainer() {
             var builder = new ContainerBuilder();
-            builder.RegisterType(ViewModelSource.GetPOCOType(typeof(DetailViewModel))).As<IDetailViewModel>();
+            builder.RegisterType(typeof(Logger)).As(typeof(ILogger)).SingleInstance();
+            builder.RegisterType(ViewModelSource.GetPOCOType(typeof(LoggerViewModel))).AsSelf();
             builder.RegisterType(ViewModelSource.GetPOCOType(typeof(MainViewModel))).AsSelf();
             return builder.Build();
         }
