c9916ef60b20ce0c7fe1324761ab4c7ac1043b4a
Serhii Pylypchuk <serhii.pylypchuk@devexpress.com>
2026-05-17 17:23:29 +0400

    Migrate samples to IocServiceProvider/IocExtension
    Replace custom DISource MarkupExtension with the built-in
    {dxmvvm:Ioc Type=...} extension. Each sample now builds its container,
    exposes it as IServiceProvider (native impl for Ninject/SimpleInjector,
    small adapter for Autofac/DryIoc/Unity/Prism), and hands it to
    IocServiceProvider.Default.ConfigureServices(...).

--- CS/DryIocDI/App.xaml.cs
+++ CS/DryIocDI/App.xaml.cs
@@ -1,4 +1,5 @@
 using Common;
+using DevExpress.Mvvm;
 using DevExpress.Mvvm.POCO;
 using DryIoc;
 using System;
@@ -6,21 +7,23 @@ using System.Windows;
 
 namespace DryIocDI {
     public partial class App : Application {
-        IContainer container;
         protected override void OnStartup(StartupEventArgs e) {
-            base.OnStartup(e);
-            container = new Container();
+            var container = new Container();
             container.Register(typeof(IDataStorage<Person>), typeof(PersonStorage), Reuse.Singleton);
             container.RegisterMany(new Type[] { typeof(IDetailViewModel), typeof(DetailViewModel) }, ViewModelSource.GetPOCOType(typeof(DetailViewModel)), Reuse.Singleton);
             container.Register(typeof(CollectionViewModel), ViewModelSource.GetPOCOType(typeof(CollectionViewModel)));
-            DISource.Resolver = Resolve;
+            IocServiceProvider.Default.ConfigureServices(new DryIocServiceProvider(container));
+            base.OnStartup(e);
         }
-        object Resolve(Type type, object key, string name) {
-            if(type == null)
-                return null;
-            if(key != null)
-                return container.Resolve(type, key);
-            return container.Resolve(type);
+    }
+
+    class DryIocServiceProvider : IServiceProvider {
+        readonly IContainer container;
+        public DryIocServiceProvider(IContainer container) {
+            this.container = container;
+        }
+        public object GetService(Type serviceType) {
+            return container.Resolve(serviceType, IfUnresolved.ReturnDefault);
         }
     }
 }
